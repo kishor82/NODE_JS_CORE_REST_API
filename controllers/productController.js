c8a78922b9245e006ecfcd50d22ccd6333d0167f
Kishor82 <kishorrathva8298@gmail.com>
2020-10-17 10:55:57 +0530

    Added Update Products APIs.

--- controllers/productController.js
+++ controllers/productController.js
@@ -82,19 +82,26 @@ async function createProduct(req, res) {
  * @param {*} req
  * @param {*} res
  */
-async function updateProduct(req, res) {
+async function updateProduct(req, res, id) {
   try {
-    const body = await getPostData(req);
-    const { title, description, price } = JSON.parse(body);
-    const product = {
-      title,
-      description,
-      price,
-    };
-    const newProduct = await Product.create(product);
+    const product = await Product.findById(id);
 
-    res.writeHead(201, { "Content-Type": "application/json" });
-    return res.end(JSON.stringify(newProduct));
+    if (!product) {
+      res.writeHead(404, { "Content-Type": "application/json" });
+      res.end(JSON.stringify({ message: "Product Not Found" }));
+    } else {
+      const body = await getPostData(req);
+      const { title, description, price } = JSON.parse(body);
+      const productData = {
+        title: title || product.title,
+        description: description || product.description,
+        price: price || product.price,
+      };
+      const uppdProduct = await Product.update(id, productData);
+
+      res.writeHead(200, { "Content-Type": "application/json" });
+      return res.end(JSON.stringify(uppdProduct));
+    }
   } catch (error) {
     console.log(error);
   }
@@ -104,5 +111,5 @@ module.exports = Object.freeze({
   getProducts,
   getProduct,
   createProduct,
-  updateProduct
+  updateProduct,
 });
